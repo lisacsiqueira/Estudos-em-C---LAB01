d714a8187a2c5d4d312b76f137be6232c4cd566a
Lisa Costa <112580152+lisacsiqueira@users.noreply.github.com>
2022-10-13 15:31:20 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+
+// escopo gobal
+// float num1, num2, num3;
+
+
+int main(void) {
+  /*
+  //exercicio 01
+  float num;
+  printf("digite um numero: ");
+  scanf("%f", &num);
+  printf("seu numero eh: %.1f", num);
+  */
+  /*
+  //exercicio 02
+
+  printf("Digite um numero: ");
+  scanf("%f", &num1);
+  printf("Digite um numero: ");
+  scanf("%f", &num2);
+  printf("Digite um numero: ");
+  scanf("%f", &num3);
+  float subtracao = num1  - num2;
+  float mult = subtracao * num3;
+
+  printf("O resultado é: %.2f", mult);
+  */
+/*
+  //exercicio03
+
+  char nome[20];
+  float altura;
+  printf("Seu nome: ");
+  scanf("%s", &nome);
+  printf("Sua altura: ");
+  scanf("%f", &altura);
+
+  float peso_ideal = (72.7*altura) - 58;
+
+  printf("%s seu peso ideal eh: %.2f", nome, peso_ideal);
+*/
+  /*
+
+    //Desafio01
+
+  float grau_f;
+  printf("graus Fahrenhei: ");
+  scanf("%f", &grau_f);
+  float grau_c = 5 * ((grau_f-32) / 9);
+
+  printf("A temperatura %.2f°F em Celsius eh: %.2f°C", grau_f, grau_c);
+  */
+/*
+  //desafio02
+
+  float ganho_hr, hr_job,desconto_ir, desconto_inss, salario_bruto,salario_liquido, resul_ir, resul_inss;
+
+  printf("Digite valor da hr trabalhada: ");
+  scanf("%f", &ganho_hr);
+  
+  printf("Horas trabalhada: ");
+  scanf("%f", &hr_job);
+
+  
+  printf("Porcentagem descontada do IR: ");
+  scanf("%f", &desconto_ir);
+
+  printf("Porcentagem descontada do INSS: ");
+  scanf("%f", &desconto_inss);
+
+  salario_bruto = ganho_hr*hr_job;
+  resul_ir = (salario_bruto*desconto_ir)/100;
+  resul_inss = (salario_bruto*desconto_inss)/100;
+  salario_liquido = salario_bruto - resul_inss - resul_ir;
+
+  printf("+ Salário Bruto : R$ %.2f\n", salario_bruto);
+  printf("- IR (%.1f%): R$ %.2f\n", desconto_ir, resul_ir);
+  printf("- IR (%.1f%): R$ %.2f\n", desconto_inss, resul_inss);
+  printf("= Salário Liquido : R$ %.2f",salario_liquido);
+
+  */
+
+  
+  
+
+  
+
+
+
+    
+  return 0;
+}
